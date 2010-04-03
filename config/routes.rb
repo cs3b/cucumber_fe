13fb8026a3ff8fac4dafea5e3c4a04c9aae6cde1
Michał Czyż <mc@cs3b.com>
2010-04-03 12:25:45 +0200

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace :documentation do |doc|
     doc.resources :features
-    doc.resource :features_change, :only => [:show], :collection => [:push, :pull, :commit]
+    # doc.resource :features_change, :only => [:show], :collection => [:push, :pull, :commit]
   end
 end
